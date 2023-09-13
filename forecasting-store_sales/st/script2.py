ff7e54ef6bad2dc70bb3afc4ab202e117f85f997
facahto <72359515+facahto@users.noreply.github.com>
2023-09-13 10:56:20 +0700

    Add files via upload

---
+++ forecasting-store_sales/st/script2.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+
+st.write(f'''
+        <a target="_self" href="https://acc-astra-kredit.streamlit.app/">
+            <button>
+                Kembali Ke Halaman Utama
+            </button>
+        </a>
+        ''',
+        unsafe_allow_html=True)   
+
+st.write('Tidak minat karena DP tinggi')
+st.write('Untuk DP bapak/ibu bisa ambil paket SUPER HEMAT dimana DP dimulai dari 15% untuk seluruh unit tpyota dan daihatsu ')
+st.write('')
+st.write('Selain itu ada paket SPEKTA dimana DP rendah mulai 10% dan free insurance 1 tahun all risk khusus unit Avanza, Veloz, Rush')
+st.write('')
+st.write('')
+
+st.write('Tidak minat karena angsuran kecil')
+st.write('Untuk brand Toyota Bapak/Ibu bisa mendapat paket EZ-DEAL dengan angsuran stepping rendah di 2tahun pertama')
+st.write('')
+st.write('Untuk brand Daihatsu bapak/ibu bisa mendapat FREE ANGSURAN 4x untuk unit Ayla&Sigra')
+st.write('')
+st.write('')
+
+st.write('Tidak minat karena benefit yang diberikan hanya itu saha')
+st.write('Ijin setelah ini saya kirimkan undangan event yang diselenggarakan oleh cabang ACC, bapak/ibu bisa claim benefit tambahannya. Selain mendapat benefit dari HO, bapak/ibu juga mendapat benefit dari event dealer seperti doorprize & merchandise')
